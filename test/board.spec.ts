1519b5e7c9a13253bf664c9c0aa69676c2200e63
Gianpiero Bozza <gianpiero.bozza@gmail.com>
2022-04-25 08:30:02 +0200

    Delete board.spec.ts

--- test/board.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-
-describe('Tile', () => {
-    describe('when asking to create a new one', () => {
-        it('checking default options', () => {
-            expect(1).to.be.equal(1);
-        });
-    });
-});
